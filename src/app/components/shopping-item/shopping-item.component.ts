55f154ae0cae01e2d03149437941609c656e090d
Ventzeslav Petrov <Ventzeslavpetrov@gmail.com>
2019-03-22 04:47:21 +0200

    Uploading latest work

--- src/app/components/shopping-item/shopping-item.component.ts
+++ src/app/components/shopping-item/shopping-item.component.ts
@@ -1,19 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { shoppingItem } from './shopping-item.model';
+import { Component, OnInit } from "@angular/core";
+import { shoppingItem } from "./shopping-item.model";
 
 @Component({
-  selector: 'app-shopping-item',
-  templateUrl: './shopping-item.component.html',
-  styleUrls: ['./shopping-item.component.css']
+  selector: "app-shopping-item",
+  templateUrl: "./shopping-item.component.html",
+  styleUrls: ["./shopping-item.component.css"]
 })
 export class ShoppingItemComponent implements OnInit {
   
-  constructor() {
-    
-   }
 
-  ngOnInit() {
-    
-  }
+  shoppingItems: shoppingItem[] = [
+    new shoppingItem(
+      "TestName",
+      "TestCollection",
+      "TestSubcollection",
+      "./images/watch1.png",
+      5
+    ),
+    new shoppingItem(
+      "TestName",
+      "TestCollection",
+      "TestSubcollection",
+      "./images/watch1.png",
+      5
+    ),
+    new shoppingItem(
+      "TestName",
+      "TestCollection",
+      "TestSubcollection",
+      "./images/watch1.png",
+      5
+    ),
+    new shoppingItem(
+      "TestName",
+      "TestCollection",
+      "TestSubcollection",
+      "./images/watch1.png",
+      5
+    ),
+    new shoppingItem(
+      "TestName",
+      "TestCollection",
+      "TestSubcollection",
+      "./images/watch1.png",
+      5
+    ),
+    new shoppingItem(
+      "TestName",
+      "TestCollection",
+      "TestSubcollection",
+      "./images/watch1.png",
+      5
+    )
+  ];
+  constructor() {}
 
+  ngOnInit() {}
 }
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -2,15 +2,13 @@ import { Component, OnInit } from "@angular/core";
 
 import { shoppingItem } from "../shopping-item/shopping-item.model";
 
+
 @Component({
   selector: "app-shopping-list",
   templateUrl: "./shopping-list.component.html",
   styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
-shoppingList: shoppingItem[] = [
-  new shoppingItem('TEST NAME', 'TEST COLLECTION', 'TEST SUBCOLLECTION', 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiquqm-jY7hAhUJ2OAKHY3ACqwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.jomashop.com%2Fomega-watch-21230412003001.html&psig=AOvVaw2wkgwS-X_lr_jo2TnH966k&ust=1553081473157499', 1000)
-]
 
   constructor() {}
 
